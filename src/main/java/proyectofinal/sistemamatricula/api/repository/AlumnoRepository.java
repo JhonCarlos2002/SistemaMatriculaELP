2c13ec55e38fe499b322f2c7e0f25c8415048ca3
AngeloPianto9 <73825828@elp.edu.pe>
2023-05-05 12:25:53 -0500

    Merge remote-tracking branch 'SistemaMatriculaELP/master'
    # Conflicts:
    #	src/main/java/proyectofinal/sistemamatricula/api/model/Detallematricula.java

--- src/main/java/proyectofinal/sistemamatricula/api/repository/AlumnoRepository.java
+++
@@ -1,7 +0,0 @@
-package proyectofinal.sistemamatricula.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import proyectofinal.sistemamatricula.api.model.Alumno;
-
-public interface AlumnoRepository extends JpaRepository<Alumno, Integer> {
-}
--- src/main/java/proyectofinal/sistemamatricula/api/repository/MatriculaRepository.java
+++ src/main/java/proyectofinal/sistemamatricula/api/repository/MatriculaRepository.java
@@ -1,7 +1,6 @@
 package proyectofinal.sistemamatricula.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import proyectofinal.sistemamatricula.api.model.Matricula;
 
 public interface MatriculaRepository extends JpaRepository<Matricula,Integer> {
 }
