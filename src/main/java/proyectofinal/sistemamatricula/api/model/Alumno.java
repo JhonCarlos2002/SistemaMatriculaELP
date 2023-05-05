4ae5a44d8ad75be841424e18c72fb05258cb06be
201-28 <royielshimon@gmail.com>
2023-05-05 08:14:46 -0500

    mi primer commit del proyecto

--- src/main/java/proyectofinal/sistemamatricula/api/model/Alumno.java
+++ src/main/java/proyectofinal/sistemamatricula/api/model/Alumno.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import java.util.Date;
 @Data
 @Entity
+@Table(name = "alumno")
 public class Alumno {
 
     @Id
--- src/main/java/proyectofinal/sistemamatricula/api/model/Cursos.java
+++ src/main/java/proyectofinal/sistemamatricula/api/model/Cursos.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 @Entity
 @Data
+@Table(name="cursos")
 public class Cursos {
 
     @Id
