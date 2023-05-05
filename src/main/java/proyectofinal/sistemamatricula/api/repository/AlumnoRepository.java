17be75992d4cd88f34776419d2fa4f69655aad83
AngeloPianto9 <73825828@elp.edu.pe>
2023-05-05 11:53:43 -0500

    mi primer commit del proyecto

--- src/main/java/proyectofinal/sistemamatricula/api/repository/AlumnoRepository.java
+++ src/main/java/proyectofinal/sistemamatricula/api/repository/AlumnoRepository.java
@@ -1,7 +1,6 @@
 package proyectofinal.sistemamatricula.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import proyectofinal.sistemamatricula.api.model.Alumno;
 
 public interface AlumnoRepository extends JpaRepository<Alumno, Integer> {
 }
