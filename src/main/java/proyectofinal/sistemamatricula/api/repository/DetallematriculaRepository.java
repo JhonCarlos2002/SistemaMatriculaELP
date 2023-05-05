c599fa6cdabb8798c1f5de82393257250d99be12
JHON CARLOS <77822462@elp.edu.pe>
2023-05-05 11:42:21 -0500

    mi primer commit del proyecto

--- src/main/java/proyectofinal/sistemamatricula/api/repository/DetallematriculaRepository.java
+++
@@ -1,7 +0,0 @@
-package proyectofinal.sistemamatricula.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import proyectofinal.sistemamatricula.api.model.Detallematricula;
-
-public interface DetallematriculaRepository extends JpaRepository<Detallematricula,Integer> {
-}
