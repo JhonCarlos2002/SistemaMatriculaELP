efcd9180cfe7309efbe517766f5b6e52061a0988
JHON CARLOS <77822462@elp.edu.pe>
2023-05-05 10:02:45 -0500

    mi primer commit del proyecto

--- src/main/java/proyectofinal/sistemamatricula/api/model/Matricula.java
+++ src/main/java/proyectofinal/sistemamatricula/api/model/Matricula.java
@@ -4,14 +4,22 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.DateTimeException;
+import java.time.LocalDateTime;
+
 @Data
 @Entity
+@Table(name = "matricula")
 public class Matricula {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idmatricula")
     private Integer id;
-    private DateTimeException fechaderegistro;
+    private LocalDateTime fechaderegistro;
     private  Double costo;
+
+    @ManyToOne
+    @JoinColumn(name = "idalumno",referencedColumnName = "idalumno",nullable = false)
+    private Alumno alumno;
+
 }
