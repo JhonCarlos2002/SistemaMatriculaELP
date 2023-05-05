a7e43e64c10e96d18ef611e0abf0c1e729ecbd17
AngeloPianto9 <73825828@elp.edu.pe>
2023-05-05 11:13:44 -0500

    mi primer commit del proyecto

--- src/main/java/proyectofinal/sistemamatricula/api/model/Detallematricula.java
+++ src/main/java/proyectofinal/sistemamatricula/api/model/Detallematricula.java
@@ -1,6 +1,31 @@
 package proyectofinal.sistemamatricula.api.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "detallematricula")
 public class Detallematricula {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JoinColumn(name = "iddetallematricula")
+    private Integer idDetallematricula;
+
+    @ManyToOne
+    @JoinColumn(name = "idcursos",referencedColumnName = "idcursos",nullable = false)
+    private Cursos cursos;
+
+    @ManyToOne
+    @JoinColumn(name = "idmatricula",referencedColumnName = "idmatricula",nullable = false)
+    private Matricula matricula;
+
+    @ManyToOne
+    @JoinColumn(name = "idalumno",referencedColumnName = "idalumno",nullable = false)
+    private Alumno alumno;
+
+
+
 
 
 }
--- src/test/java/proyectofinal/sistemamatricula/api/ApplicationTests.java
+++ src/test/java/proyectofinal/sistemamatricula/api/ApplicationTests.java
@@ -1,13 +1,14 @@
 package proyectofinal.sistemamatricula.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ApplicationTests {
+
 
 	@Test
 	void contextLoads() {
 	}
-
 }
