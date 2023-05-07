e1c1ad5ca106086f371c23aeba12099c46b0f5f2
JHON CARLOS <77822462@elp.edu.pe>
2023-05-07 14:22:10 -0500

    mi primer commit del proyecto

--- src/main/java/proyectofinal/sistemamatricula/api/repository/CursosRepository.java
+++ src/main/java/proyectofinal/sistemamatricula/api/repository/CursosRepository.java
@@ -1,6 +1,7 @@
 package proyectofinal.sistemamatricula.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import proyectofinal.sistemamatricula.api.model.Cursos;
 
 public interface CursosRepository extends JpaRepository<Cursos,Integer> {
 
