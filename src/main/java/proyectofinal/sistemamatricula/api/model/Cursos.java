469fef2cef07ccd54f965d898112db58ca50e7b2
JHON CARLOS <77822462@elp.edu.pe>
2023-05-07 14:05:45 -0500

    mi primer commit del proyecto

--- src/main/java/proyectofinal/sistemamatricula/api/model/Cursos.java
+++ src/main/java/proyectofinal/sistemamatricula/api/model/Cursos.java
@@ -10,9 +10,13 @@ public class Cursos {                   //Sin miedo al exito muchachitos
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idcursos")
-    private Integer id;
+    private Integer idCursos;
+    private String  codigocurso;
     private String nombrecurso;
+    private String  sede;
     private Integer credito;
+    private String ciclo;
+    private String seccion;
 
 
 }
